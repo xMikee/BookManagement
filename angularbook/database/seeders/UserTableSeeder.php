b3dcddc7903fe5f9d4499ba1cf57f82a997f91f5
support <support@dfsolution.it>
2024-08-07 17:30:59 +0200

    Add Users Seeder

--- angularbook/database/seeders/UserTableSeeder.php
+++ angularbook/database/seeders/UserTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 1; $i <= 10; $i ++) {
+            User::factory()->create([
+                'name' => "Test User {$i}",
+                'email' => "test{$i}@example.com",
+                'password' => bcrypt('password')
+            ]);
+        }
     }
 }
